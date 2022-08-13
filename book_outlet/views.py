9654538614d238d7b8aa0bb65009110d56b5020a
coder-dilip <designsdilip@gmail.com>
2022-08-13 09:56:11 +0545

    added slug

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
+from .models import Book
 # Create your views here.
 def index(request):
-    return render(request,"book_outlet/index.html")
+    books=Book.objects.all()
+    return render(request,"book_outlet/index.html",{
+        "books":books
+    })
+
+def book_detail(request,slug):
+    try:
+        book=Book.objects.get(slug=slug)
+        return render(request, "book_outlet/book_detail.html",{
+            "title":book.title,
+            "author":book.author,
+            "rating":book.rating,
+            "is_bestselling_book":book.is_bestselling_book,
+            "slug":slug
+        })
+    except Exception:
+        raise Http404()
     
